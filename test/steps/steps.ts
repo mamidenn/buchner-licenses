5c16a881d327cf9ab2c550de8c27a7069b26f835
Martin Dennhardt <martin@dennhardt.de>
2021-08-01 07:41:35 +0000

    Make cucumber work

---
+++ test/steps/steps.ts
@@ -0,0 +1,20 @@
+import { Given, Then, When } from "@cucumber/cucumber";
+import assert from "assert";
+
+Given("a variable set to {int}", function (int) {
+  // Given('a variable set to {float}', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("I increment the variable by {int}", function (int) {
+  // When('I increment the variable by {float}', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("the variable should contain {int}", function (int) {
+  // Then('the variable should contain {float}', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});

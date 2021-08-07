b316758c54404754cf6eb1f2fcd6d6ae4a2465ed
Martin Dennhardt <martin@dennhardt.de>
2021-08-07 11:23:26 +0000

    Fix data fetching

--- pages/api/licensebundleoffer.ts
+++ pages/api/licensebundleoffer.ts
@@ -1,70 +1,38 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import Dinero from "dinero.js";
 import { createReadStream } from "fs";
-import {readFile} from "fs/promises"
 import type { NextApiRequest, NextApiResponse } from "next";
 import csvParser from "csv-parser";
-import path from "path";
 
-type License = {
-  name: string;
-  price: Dinero.Dinero;
-  users: number;
+type LicensePrices = {
+  Einzellizenz: number;
+  kleine_FS_Lizenz: number;
+  mittlere_FS_Lizenz: number;
+  große_FS_Lizenz: number;
+  Floating_Lizenz: number;
 };
 
-const einzel: License = {
-  name: "Einzellizenz",
-  price: Dinero({ amount: 2280, currency: "EUR" }),
-  users: 1,
-};
+type LicensePriceDict = { [Berechnungsformel: number]: LicensePrices };
 
-type LicenseBundle = {
-  licenses: [License];
-};
-
-type LicenseBundleOffer = {
-  exact: LicenseBundle;
-  cheapest?: LicenseBundle;
-  recommended: LicenseBundle;
-};
+const getPrices = () =>
+  new Promise<LicensePriceDict>((resolve) => {
+    const results: LicensePriceDict = {};
+    createReadStream("public/data/license-bundles.csv")
+      .pipe(
+        csvParser({
+          mapHeaders: ({ header }) => header.trim(),
+          mapValues: ({ value }) => parseFloat(value.trim()),
+        })
+      )
+      .on("data", ({ Berechnungsformel, ...data }) => {
+        results[Berechnungsformel] = data;
+      })
+      .on("end", () => resolve(results));
+  });
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<LicenseBundleOffer>
+  res: NextApiResponse<LicensePriceDict>
 ) {
-  const results = [];
-
-  const data = await readFile(path.resolve("./public", "data/license-bundles.csv")
-
-  createReadStream(path.resolve("./public", "data/license-bundles.csv"))
-    .pipe(
-      csvParser({
-        mapHeaders: ({ header }) => header.trim(),
-        mapValues: ({ value }) => parseFloat(value.trim()),
-      })
-    )
-    .on("data", (data) => results.push(data))
-    .on("end", () => {
-      console.log(results);
-    });
-  res.status(200).json({
-    exact: {
-      licenses: [
-        {
-          name: "Einzellizenz",
-          price: Dinero({ amount: 2280, currency: "EUR" }),
-          users: 1,
-        },
-      ],
-    },
-    recommended: {
-      licenses: [
-        {
-          name: "Einzellizenz",
-          price: Dinero({ amount: 2280, currency: "EUR" }),
-          users: 1,
-        },
-      ],
-    },
-  });
+  const prices = await getPrices();
+  res.status(200).json(prices);
 }
